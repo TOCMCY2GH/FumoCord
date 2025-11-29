3007f11f993146ab2914ac6fbda917532023cf52
TOCMCY2GH <resend-yelp-rubber@duck.com>
2025-11-29 07:54:03 +0000

    Update client.dart

--- bin/client.dart
+++ bin/client.dart
@@ -5,7 +5,7 @@ void main() async {
   Socket? socket;
   for (var i = 1; i <= 10; i++) {
     try {
-      socket = await Socket.connect("127.0.0.1", 4040); // 192.168.1.1
+      socket = await Socket.connect("127.0.0.1", 4040);
       break;
     } catch (e) {
       print("Failed to connect to the chat server ᗜ⁔ᗜ, retrying...)");
